1952c4fd8cf893dcbdcda28a0fae3d1ab0ed80f2
weijia <james.wei.pek@gmail.com>
2015-02-20 14:50:35 +0800

    add functions to sync files from gridfs to amazon s3.

--- gridfs2s3.go
+++ gridfs2s3.go
@@ -6,36 +6,38 @@ import (
 	"log"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/s3"
-	"gopkg.in/mgo.v2"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 )
 
 var (
 	access_key string
 	secret_key string
 	region     string
-	bucket     string
-
 	mongo_url  string
 	mongo_db   string
 	mongo_coll string
+	workers    int
+	date       string
+)
 
-	workers int
+const (
+	DATE_FORM = "2006-01-02"
 )
 
 func init() {
-	flag.StringVar(&access_key, "k", "", "AWS access key")
-	flag.StringVar(&secret_key, "s", "", "AWS secret key")
-	flag.StringVar(&region, "r", "us-east-1", "AWS region")
-	flag.StringVar(&bucket, "b", "", "S3 bucket for the files")
-
-	flag.StringVar(&mongo_url, "h", "mongodb://localhost", "MongoDB connection string (e.g. mongodb://host1:port1,host2:port2)")
-	flag.StringVar(&mongo_db, "d", "", "MongoDB database name")
-	flag.StringVar(&mongo_coll, "c", "", "Prefix of MongoDB collection to migrate. Default is to migrate everything. Use full name to migrate a single collection")
-
-	flag.IntVar(&workers, "w", 1, "Number of parallel workers. 2 x GOMAXPROCS seems to work well")
+	flag.StringVar(&access_key, "k", "", "access key")
+	flag.StringVar(&secret_key, "s", "", "secret key")
+	flag.StringVar(&region, "r", "us-east-1", "region")
+	flag.StringVar(&date, "t", "", "start date")
+	flag.StringVar(&mongo_url, "h", "mongodb://localhost", "MongoDB endpoint (e.g. mongodb://host1:port1,host2:port2)")
+	flag.StringVar(&mongo_db, "d", "file", "Gridfs database name")
+	flag.StringVar(&mongo_coll, "c", "fs", "Gridfs collection name")
+	flag.IntVar(&workers, "w", 1, "parallel workers")
 }
 
 func main() {
@@ -46,95 +48,109 @@ func main() {
 	auth, err := aws.GetAuth(access_key, secret_key)
 	check(err)
 	client := s3.New(auth, aws.Regions[region])
-	bucket := client.Bucket(bucket)
-
-	fmt.Println("Getting the list of existing keys")
-	existing, err := bucket.GetBucketContents()
-	check(err)
-
+	image_bucket := client.Bucket("mico-image")
+	video_bucket := client.Bucket("mico-video")
+	audio_bucket := client.Bucket("mico-audio")
 	fmt.Println("Preparing MongoDB client")
 	session, err := mgo.Dial(mongo_url)
 	check(err)
-	db := session.DB(mongo_db)
+	defer session.Close()
 
-	fmt.Println("Getting list of image collections")
-	collections, err := db.CollectionNames()
+	begin, err := time.Parse(DATE_FORM, date)
 	check(err)
+	dateRange := make([]time.Time, workers+1)
+	dateRange[0] = begin
+	split := 24 / workers
+	for i := 0; i < workers; i++ {
+		dateRange[i+1] = begin.Add(time.Duration(split*(i+1)) * time.Hour)
+	}
 
-	// Migrate files
-	for _, collection := range collections {
-		// Skip non-files collections
-		if !strings.HasSuffix(collection, "files") {
-			continue
-		}
-		// Use user specified filter if provided
-		if mongo_coll != "" && !strings.HasPrefix(collection, mongo_coll) {
-			continue
-		}
+	// // Migrate files
+	// gfs := session.DB(mongo_db).GridFS(mongo_coll)
+	// iter := gfs.Find(bson.M{"uploadDate": bson.M{"$gte": begin, "$lte": end}}).Iter()
+	// total, err := gfs.Find(bson.M{"uploadDate": bson.M{"$gte": begin, "$lte": end}}).Count()
+	// check(err)
+	// fmt.Println("Migrating", total, "files")
 
-		prefix := strings.TrimSuffix(collection, ".files")
-		gfs := db.GridFS(prefix)
-		files := gfs.Find(nil).Iter()
+	migrated := make(chan int, workers)
+	wg := new(sync.WaitGroup)
+	// wg.Add(workers)
 
-		total_count, _ := gfs.Find(nil).Count()
-		fmt.Println("Migrating", total_count, "images for", prefix)
-
-		migrated := make(chan int, workers)
-		wg := new(sync.WaitGroup)
-		wg.Add(workers)
-
-		for i := 0; i < workers; i++ {
-			go func(id int) {
-				var f *mgo.GridFile
-				file_count := 0
-
-				for gfs.OpenNext(files, &f) {
-					path := prefix + "/" + f.Name()
-
-					// Skip if the file exist and is the same as in mongo
-					// ETag includes quotation marks ...
-					if key, exists := (*existing)[path]; exists && key.ETag[1:33] == f.MD5() {
-						log.Println(id, "SKIPPING", path)
-						file_count++
-						continue
-					}
-
-					log.Println(id, "INSERTING", path)
-					err := bucket.PutReader(path, f, f.Size(), f.ContentType(), s3.BucketOwnerFull)
-					if err != nil {
-						log.Println(id, "INSERT ERROR", err)
-						continue
-					}
-
-					file_count++
-					if file_count%100 == 0 {
-						migrated <- 100
-					}
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			sess := session.Copy()
+			defer sess.Close()
+			gfs := sess.DB(mongo_db).GridFS(mongo_coll)
+			total, err := gfs.Find(bson.M{"uploadDate": bson.M{"$gte": dateRange[id], "$lte": dateRange[id+1]}}).Count()
+			check(err)
+			fmt.Println("migrating", total, "files, from", dateRange[id], "to", dateRange[id+1])
+			iter := gfs.Find(bson.M{"uploadDate": bson.M{"$gte": dateRange[id], "$lte": dateRange[id+1]}}).Iter()
+			defer iter.Close()
+			var f *mgo.GridFile
+			file_count := 0
+			for gfs.OpenNext(iter, &f) {
+				log.Println("file:", f.Name(), f.ContentType(), f.Size(), f.UploadDate())
+				name := f.Name()
+				if strings.HasSuffix(name, "_t") || strings.HasSuffix(name, "_thumbnail") {
+					log.Println("ignore", name)
+					continue
 				}
-
-				wg.Done()
-			}(i)
-		}
-
-		done := make(chan bool)
-		go func() {
-			migrated_count := 0
-			for m := range migrated {
-				migrated_count += m
-				if migrated_count%1000 == 0 {
-					fmt.Println("Migrated", migrated_count, "/", total_count, "images for", prefix)
+				contentType := f.ContentType()
+				if contentType == "" {
+					log.Println("missing type", name)
+					contentType = "image/png"
+				}
+				if strings.HasPrefix(contentType, "audio") {
+					err := audio_bucket.PutReader(name, f, f.Size(), contentType, s3.PublicRead)
+					if err != nil {
+						log.Println(id, "upload error", err)
+						continue
+					}
+					log.Println("upload", name, contentType)
+					file_count++
+				} else if strings.HasPrefix(contentType, "video") {
+					err := video_bucket.PutReader(name, f, f.Size(), contentType, s3.PublicRead)
+					if err != nil {
+						log.Println(id, "upload error", err)
+						continue
+					}
+					log.Println("upload", name, contentType)
+					file_count++
+				} else if strings.HasPrefix(contentType, "image") {
+					err := image_bucket.PutReader(name, f, f.Size(), contentType, s3.PublicRead)
+					if err != nil {
+						log.Println(id, "upload error", err)
+						continue
+					}
+					log.Println("upload", name, contentType)
+					file_count++
+				} else {
+					log.Println("invalid type", name, contentType)
+					continue
 				}
 			}
-			fmt.Println("Migrated", total_count, "/", total_count, "images for", prefix)
-			done <- true
-		}()
-
-		wg.Wait()
-		close(migrated)
-		<-done
-
-		check(files.Close())
+			log.Println("error on iter", iter.Err())
+			log.Println("worker", id, "migrated", file_count, "files, from", dateRange[id], "to", dateRange[id+1])
+			migrated <- file_count
+		}(i)
 	}
+
+	done := make(chan bool)
+	go func() {
+		count := 0
+		for m := range migrated {
+			count += m
+		}
+		fmt.Println("totally migrated", count, "files")
+		done <- true
+	}()
+
+	wg.Wait()
+	close(migrated)
+	<-done
+	// check(iter.Close())
 }
 
 func check_args() {
@@ -144,14 +160,14 @@ func check_args() {
 	if _, ok := aws.Regions[region]; !ok {
 		log.Fatal("Invalid region name")
 	}
-	if bucket == "" {
-		log.Fatal("Bucket name is require")
-	}
 	if mongo_url == "" {
 		log.Fatal("MongoDB connection string is required")
 	}
 	if mongo_db == "" {
 		log.Fatal("MongoDB database name is required")
+	}
+	if mongo_coll == "" {
+		log.Fatal("MongoDB collection name is required")
 	}
 }
 
